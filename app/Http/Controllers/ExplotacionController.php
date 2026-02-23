cb6d47fc18fe98d2598ce845f500649ad5d34142
AlComOl <alvcomoli@alu.edu.gva.es>
2026-02-23 13:24:41 +0100

    quitado el required del controlador de usuario y propietario

--- app/Http/Controllers/ExplotacionController.php
+++ app/Http/Controllers/ExplotacionController.php
@@ -63,8 +63,8 @@ public function resumenExplotaciones(){
             'nombre' => 'required|max:25',
             'ubicacion' => 'required',
             'descripcion' => 'required',
-            'user_id' => 'required',
-            'propietario_id' => 'required',
+            'user_id' => 'nullable',
+            'propietario_id' => 'nullablegit ',
             ]);
 
              Explotacion::create($explotacion);
